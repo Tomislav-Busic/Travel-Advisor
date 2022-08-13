9b1a3b663acb6667ea0200b409e3d42901c05beb
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-13 14:33:03 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    return (
+        <div>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
+
+/*
+xs={12} this means that this grid is going to take full width on mobile devices
+md={4} on medium and larger devices only going to take 4 spaces
+*/
 
 export default App;
