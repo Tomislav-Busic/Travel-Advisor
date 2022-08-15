b94232eef916fbeeb10bd4aae18ba10da4134283
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-15 18:53:16 +0200

    bounds latitude longitude

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
-import {getPlacesData} from './api/index';
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
@@ -10,15 +10,21 @@ const App = () => {
     const [ places, setPlaces ] = useState([]);
 
     const [ coordinates, setCoordinates ] = useState({});
-    const [ bounds, setBounds ] = useState(null)
+    const [ bounds, setBounds ] = useState({})
 
     useEffect(() => {
-        getPlacesData()
-        .then((data) => {
-            console.log(data)
-            setPlaces(data)
-        })
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        });
     }, []);
+
+    useEffect(() => {
+        getPlacesData(bounds.sw, bounds.ne)
+        .then((data) => {
+            console.log(data);
+            setPlaces(data);
+        })
+    }, [coordinates, bounds]);
 
     return (
         <div>
@@ -26,7 +32,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{width: '100%', margin: '0px'}}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
